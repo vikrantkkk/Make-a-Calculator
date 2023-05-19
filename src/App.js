32ff7d9a715e4d36ab4d2876cdca8bd600c20117
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-05-19 09:17:24 +0530

    files modified but not completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <div className="inputArea">
+        <input />
+      </div>
+      <div className="speacalRow" >
+        <button>c</button>
+        <button>%</button>
+        <button>/</button>
+      </div>
+      <div className="sevnin">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+      <div className="forsix">
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+      </div>
+
+      <div className="onethree">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+
+      <div className="zero">
+        <button>0</button>
+        <button>.</button>
+      </div>
+      <div className="speacalColumn">
+        <button>*</button>
+        <button>-</button>
+        <button>+</button>
+        <button>=</button>
+      </div>
     </div>
   );
 }
