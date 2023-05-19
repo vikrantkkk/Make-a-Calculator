60bc7e2c5da53597d33a0c41a535c9610674f7ab
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-05-19 12:57:53 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [data, setData] = useState('');
+
+  const handleChange = (e) => {
+    setData(data.concat(e.target.value));
+  };
+
+  const handleClear = () => {
+    setData('');
+  };
+
+  const calculate = () => {
+    setData(eval(data).toString());
+  };
+
   return (
     <div className="container">
-      <div className="inputArea">
-        <input />
-      </div>
-      <div className="speacalRow" >
-        <button>c</button>
-        <button>%</button>
-        <button>/</button>
-      </div>
-      <div className="sevnin">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-      </div>
-      <div className="forsix">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-      </div>
-
-      <div className="onethree">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-      </div>
-
-      <div className="zero">
-        <button>0</button>
-        <button>.</button>
-      </div>
-      <div className="speacalColumn">
-        <button>*</button>
-        <button>-</button>
-        <button>+</button>
-        <button>=</button>
-      </div>
+      <input
+        className="inputArea"
+        id="inputvalues"
+        placeholder="0"
+        value={data}
+      />
+      <input
+        type="button"
+        value="9"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="8"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="7"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="6"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="5"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="4"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="3"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="2"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="1"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="0"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="."
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="-"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="+"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="/"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="%"
+        className="button"
+        onClick={handleChange}
+      />
+      <input
+        type="button"
+        value="clear"
+        className="button button1"
+        onClick={handleClear}
+      />
+      <input
+        type="button"
+        value="="
+        className="button button1"
+        onClick={calculate}
+      />
     </div>
   );
 }
